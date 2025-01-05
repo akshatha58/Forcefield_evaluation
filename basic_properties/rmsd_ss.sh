cec35cf1fb1ae45c03d9b011a0f379b47cf57db5
Akshatha <141127837+akshatha58@users.noreply.github.com>
2025-01-05 17:42:46 +0530

    Create rmsd_ss.sh

---
+++ basic_properties/rmsd_ss.sh
@@ -0,0 +1,28 @@
+# rmsd_ss.sh
+# 4 December 2024
+
+path=$1
+pdb=$(basename $path)
+pdb=${pdb%.pdb}
+
+process_trajpath=$path/Analysis/processed_trajs
+results_path=$path/Analysis/basic_properties
+
+# # Group residues according to assigned secondary structure
+python3 ndx_groups.py $path
+
+cd $path
+/usr/local/gromacs/bin/gmx make_ndx -f md.gro -o index.ndx
+
+# Index grouping syntaxes (sample: type the following in the index file, and uncomment the last three lines of code)
+# r 11-15                   residue numbers 11 to 15 (get groups from groups.txt)
+# name 17 helices           name group 10 as helices
+# name 18 sheets
+# !17 & !18 & 1             flexible groups in the protein
+# name 19 flexible
+# q
+
+# Crosscheck group numbers before executing
+# printf "18 18" | /usr/local/gromacs/bin/gmx rms -f md.xtc -s md.tpr -o $results_path/rmsd_helix.xvg -tu ns -n index.ndx
+# printf "19 19" | /usr/local/gromacs/bin/gmx rms -f md.xtc -s md.tpr -o $results_path/rmsd_sheet.xvg -tu ns -n index.ndx
+# printf "20 20" | /usr/local/gromacs/bin/gmx rms -f md.xtc -s md.tpr -o $results_path/rmsd_flexible.xvg -tu ns -n index.ndx
