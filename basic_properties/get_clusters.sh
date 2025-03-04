d78740a7bfd9017e69240a2ae8221b55164e9b17
Akshatha <141127837+akshatha58@users.noreply.github.com>
2025-03-04 10:20:47 +0530

    Update get_clusters.sh

--- basic_properties/get_clusters.sh
+++ basic_properties/get_clusters.sh
@@ -3,16 +3,21 @@
 
 # ./get_clusters.sh <directory path>
 
+# Parameters for clustering (change accordingly)
+cluster_method="gromos"
+cluster_cutoff=0.25
+
 dir=$1
 pdb=$(basename $dir)
 resultsdir=$dir/Analysis
 mkdir $resultsdir/clusters
+
 GROMACS=/usr/local/gromacs/bin/gmx
 
 trajfile=$resultsdir/processed_trajs/$pdb"_md_trajmod.xtc"
 structfile=$resultsdir/processed_trajs/$pdb"_md_mod.gro"
 indexfile=$dir/index.ndx
 
-printf "1 1" | $GROMACS cluster -f $trajfile -s $structfile -method gromos -cutoff 0.25 -g $resultsdir/clusters/$pdb"_clusters.log" -cl $resultsdir/clusters/$pdb"_clusters.pdb" -n $indexfile
+printf "1 1" | $GROMACS cluster -f $trajfile -s $structfile -method $cluster_method -cutoff $cluster_cutoff -g $resultsdir/clusters/$pdb"_clusters.log" -cl $resultsdir/clusters/$pdb"_clusters.pdb" -n $indexfile
 mv rmsd-clust.xpm $resultsdir/clusters/$pdb"_rmsd-clust.xpm"
 mv rmsd-dist.xvg $resultsdir/clusters/$pdb"_rmsd-dist.xvg"
