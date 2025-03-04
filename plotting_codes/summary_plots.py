4242442135c6c0f82a7fe32f1611e52f57435020
Akshatha <141127837+akshatha58@users.noreply.github.com>
2025-03-04 12:09:31 +0530

    Update summary_plots.py

--- plotting_codes/summary_plots.py
+++ plotting_codes/summary_plots.py
@@ -7,11 +7,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import sys
+import os
 
+num_resids = {'1r36':140, '2l6b':115,'1ozi':99,'2lro':172,'2jxy':194,'2k0q':74,'2m68':142,'2gmo':83,'1z9b':135,'2l4x':128,'4bwh':88,'2juo':89,'2lht':122}
 
-pdb_listmid = ['4bwh', '2l6b', '2k0q']
+pdb_listmid = ['4bwh', '2l6b', '2k0q', '2gmo']
 pdb_listhigh = ['1z9b', '2ec7', '2lht', '2l4x']
-pdb_listlow = ['1ozi', '1r36', '2gmo', '2juo', '2jxy', '2lro', '2m68']
+pdb_listlow = ['1ozi', '1r36', '2juo', '2jxy', '2lro', '2m68']
+
+allproteins = pdb_listmid + pdb_listhigh + pdb_listlow
+pdb_both_ff = ['1ozi', '1r36', '2gmo', '2k0q']
+
 dir = sys.argv[1]
 
 plt.rcParams['font.size'] = 25
@@ -33,14 +39,26 @@ def colorfold(pdb):
         return 'blue'
     else:
         return 'orange'
-    
-    
+       
+def plot_ffprop(ff_file, pdb, prop, cols):
+    if os.path.isfile(ff_file):
+        data_amber = pd.read_csv(ff_file, sep="\s+", names=cols, skiprows=18)
+        time = data_amber['time']
+        yaxis_amber = data_amber['property']
+        print(pdb, " Amber")
+        print("Average"+prop+": ",np.mean(yaxis_amber))
+        print("Std Dev"+prop+": ",np.std(yaxis_amber))
+        if prop == 'rmsd':
+            rmsd_100 = np.mean(yaxis_amber)/(1+np.log(num_resids[pdb]/100))
+            print("Normalised RMSD_100: ",rmsd_100)
+        plt.plot(time, yaxis_amber, linewidth=1.5, label=pdb)
+
 def plot(pdblist, prop, plotfile):
     """
     Plots structural properties for proteins
     """
     iter = 0
-    fig, ax = plt.subplots(len(pdblist), 1, figsize=(15, 15))
+    fig, ax = plt.subplots(len(pdblist), 1, figsize=(17, 15))
     if prop == "rmsd":
         # plt.suptitle("RMSD (Backbone)", fontsize=30)
         cols = ['time', 'property']
@@ -51,22 +69,27 @@ def plot(pdblist, prop, plotfile):
     for pdb in pdblist:
         colour = colorfold(pdb)
         iter += 1
-        filename = dir+pdb+"/Analysis/basic_properties/"+prop+".xvg"
-        data = pd.read_csv(filename, sep="\s+", names=cols)
-        time = data['time']
-        yaxis = data['property']
-        # xorient = data['propx']
-        # yorient = data['propy']
-        # zorient = data['propz']
 
+        # Extracting property information from Amber force field
+        # for ss in ['helix', 'sheet', 'flexible']:
+        filename_amber = dir+"/Final_simfiles/"+pdb+"/Analysis_equi/basic_properties/"+prop+".xvg"
         plt.subplot(len(pdblist), 1, iter)
-        plt.plot(time, yaxis, linewidth=1.5, label=pdb, color=colour)
-        # plt.plot(time, xorient, label='x')
-        # plt.plot(time, yorient, label='y')
-        # plt.plot(time, zorient, label='z')
-        # plt.ylabel("RMSD (nm)", fontsize=16)
-        plt.grid('both')
-        plt.legend(fontsize=25, loc="lower right")
+        plot_ffprop(filename_amber, pdb, prop, cols)
+        
+
+        if pdb in pdb_both_ff:
+            # For proteins where CHARMM simulations are complete
+            # for ss in ['helix', 'sheet', 'flexible']:
+            filename_charmm = dir+"/Simfiles/"+pdb+"/Analysis_equi/basic_properties/"+prop+".xvg"
+            plot_ffprop(filename_charmm, pdb, prop, cols)
+            filename_charmm_tip4p = dir+"/charmm_tip4p"+pdb+"/Analysis_equi/basic_properties/"+prop+".xvg"
+            plot_ffprop(filename_charmm_tip4p, pdb, prop, cols)
+            
+        plt.grid('y')
+        plt.legend(fontsize=25)
+
+        if iter != len(pdblist):
+            plt.xticks([])
         
     # plt.xlabel("Time (ns)", fontsize=20)
     fig.text(0.5, 0.04, 'Time (ns)', ha='center', fontsize=30)
@@ -76,13 +99,45 @@ def plot(pdblist, prop, plotfile):
         fig.text(0.04, 0.5, 'Rg (nm)', va='center', rotation='vertical', fontsize=30)
     plt.savefig("plots/"+plotfile)
 
-    
+def annotation(prop, data_amber, data_charmm, colour):
+    # for i, label in enumerate(data_charmm['Protein']):
+        # plt.annotate(label, (data_amber[prop+'_mean'].iloc[i], data_charmm[prop+'_mean'].iloc[i]), fontsize=5)
+    plt.errorbar(data_amber[prop+'_mean'], data_charmm[prop+'_mean'], yerr=data_charmm[prop+'_sd'], xerr=data_amber[prop+'_sd'], alpha=0.5, color='violet', linestyle='')
+    plt.scatter(data_amber[prop+'_mean'], data_charmm[prop+'_mean'], color=[colour, colour, colour, colour],s=20)
+   
+def plot_averages(plotfile):
+    """
+    Plots structural properties for proteins
+    """
+    cols = ['Protein',	'helix_mean',	'sheet_mean',	'flexible_mean',	'rmsd_mean', 'helix_sd',	'sheet_sd',	'flexible_sd',	'rmsd_sd',	'rg_mean',	'rg_sd']
 
+    filename_amber = dir+"/Analysis_codes/plotting_codes/plots/rgrmsd_amber.txt"
+    amber = pd.read_csv(filename_amber, sep="\s+")
+    filename_charmm = dir+"/Analysis_codes/plotting_codes/plots/rgrmsd_charmm.txt"
+    data_charmm = pd.read_csv(filename_charmm, sep="\s+")
+    data_amber = amber[amber.iloc[:, 0].isin(data_charmm['Protein'])]
 
-# plot(pdb_listhigh, "rmsd", "rmsd_high.png")
+    # annotation('helix', data_amber, data_charmm, 'red')
+    # annotation('sheet', data_amber, data_charmm, 'blue')
+    # annotation('flexible', data_amber, data_charmm, 'black')
+    # annotation('rmsd', data_amber, data_charmm, 'green')
+    annotation('rg', data_amber, data_charmm, 'black')
+
+    refline = np.arange(1,1.6,0.001)
+    plt.plot(refline, refline, "--")
+    # plt.xlim([0,1.4])
+    # plt.ylim([0,1.4])
+    plt.ylabel("Rg (nm) CHARMM")
+    plt.xlabel("Rg (nm) Amber")
+    plt.grid('both')
+    plt.savefig("plots/"+plotfile)
+
+plot(['1ozi'], "rmsd", "rmsd_1ozi.png")
 # plot(pdb_listmid, "rmsd", "rmsd_mid.png")
 # plot(pdb_listlow, "rmsd", "rmsd_low.png")
 # plot(pdb_listhigh, "rg", "rg_high.png")
 # plot(pdb_listmid, "rg", "rg_mid.png")
 # plot(pdb_listlow, "rg", "rg_low.png")
 
+
+# plot_averages('rg_all.png')
